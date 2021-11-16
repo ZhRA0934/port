6e902566d57a7e12fed0ba4065fb83d3261c556f
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-16 17:29:07 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Rustambek</title>
+  <style>
+   .rightpic {
+    float: right;
+    margin: 0 0 5px 5px;
+   }
+  </style>
+ </head>
+ <body>
+  <p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU" alt="Иллюстрация" 
+   width="200" height="272" class="rightpic">
+  Hfyffyf</p>
+  <p>hcyfh</p> 
+ </body>
+</html>
