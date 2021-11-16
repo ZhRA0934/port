4e701428431a8475e5f4e9290b817770c635a6d6
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-16 17:31:36 +0600

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,28 @@
     margin: 0 0 5px 5px;
    }
   </style>
+
+<style>
+   .parent {
+   padding: 10px;
+}
+img {
+  position: relative;
+  top: 100%; 
+  left: 100%;
+  height: 50%;
+  width: 50%;
+}
+   .round {
+    border-radius: 10000px;
+    border: px solid green;
+    box-shadow: 0 0 7px #666;
+   </style>
+
  </head>
  <body>
   <p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU" alt="Иллюстрация" 
-   width="200" height="272" class="rightpic">
+   width="200" height="272" class="round" class="rightpic">
   Hfyffyf</p>
   <p>hcyfh</p> 
  </body>
